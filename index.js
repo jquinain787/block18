082c5690e8ba7d99a95234c2e6b0114a44f940cb
jquinain787 <141972848+jquinain787@users.noreply.github.com>
2023-10-19 13:50:08 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+// multiplaication
+// Take two numebrs in user input
+// Create a function called multiply
+// Take user input as parameters
+// Return both paramters multipled by one another
+
+// Concarodds
+// Take in two arrays as user input
+// Check that both arrays are only numbers sepereated by commas
+// Write a function called odd_finder using both arrays as parameters
+// Create an empty array to store the odd numbers
+// Create a for loop that loops through both parameters storing all the odd numbers into the empty array(not yet sorted)
+// Using another for loop we then sort through the empty array and store all odds numbers in order
+// return the array 
+
+// Shopping cart
+// Ask user if they have an account
+// if user says yes take use to log in screen
+// if user says no. Ask user if they would like to make on or have the option to simply skip\
+// After user has decided to login or skip the sceen will show the user their cart
+// if the cart is empty it will just say empty
+// If not the screen will print out all the users items
+// Create a checout button where the user can see the total price of their cart
+// The user then select  a payment method
+// The user checksout
+// The computer will check if the funds went through
+// If the funds successfully go through the computer will print a recipt 
+// If not the screen will print payment declined. try again
